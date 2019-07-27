6dcf269006ad185a92ec162391fafb5bd7646cfb
Artur Markov <artur.markov1860@gmail.com>
2019-07-27 10:07:34 -0700

    div with gif and rating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
 $(document).on("click", "button", function () {
     $("#gif-view").empty();
     var gifQ = $(this).val();
-    var gifURL = "https://api.giphy.com/v1/gifs/search?api_key=30JspaJIb3w1ZakLhJQZgtog3Z26m3mp&q="+gifQ+"&limit=25&offset=0&rating=G&lang=en";
+    var gifURL = "https://api.giphy.com/v1/gifs/search?api_key=30JspaJIb3w1ZakLhJQZgtog3Z26m3mp&q="+gifQ+"&limit=25&offset=0&lang=en";
     $.ajax({
         url: gifURL,
         method: "GET"
     }).then(function(response){
         for(var i = 0; i < 11; i++) {
-            $("#gif-view").append("<img>");
-            // $("img").css("width", "400px");
-            // $("img").css("height", "300px");
-            // $("img").css("margin", "20px");
-            $("#gif-view img:last-child").attr("src", response.data[i].images.downsized_still.url);
-            $("#gif-view img:last-child").attr("data-moving", response.data[i].images.downsized_large.url);
-            $("#gif-view img:last-child").attr("data-static", response.data[i].images.downsized_still.url);
+            $("#gif-view").append("<div>");
+            $("#gif-view div:last-child").attr("class", "divWithGif");
+            $("#gif-view div:last-child").append("<img>");
+            $(".divWithGif img:last-child").attr("src", response.data[i].images.downsized_still.url);
+            $(".divWithGif img:last-child").attr("data-moving", response.data[i].images.downsized_large.url);
+            $(".divWithGif img:last-child").attr("data-static", response.data[i].images.downsized_still.url);
+            $(".divWithGif:last-child").append("<span>");
+            $(".divWithGif span:last-child").text("Rating " + response.data[i].rating);
+
         }
     });
 });

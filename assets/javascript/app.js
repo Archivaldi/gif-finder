56807588cea63b7e90d65dbb0a519ef85f7f5f3a
Artur Markov <artur.markov1860@gmail.com>
2019-07-29 20:28:28 -0700

    restrictions for adding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var limit = 10;
+var limit = 0;
 $(document).on("click", ".createGifs", function () {
     limit = 10;
     $("#gif-view").empty();
@@ -24,30 +24,34 @@ $(document).on("click", ".createGifs", function () {
 });
 
 $("#more").on("click", function () {
-    var startSearchNum = $("#gif-view").children().length;
-    limit += 10;
-    gifQ = $(".divWithGif img:first-child").eq(0).attr("value");
-    var gifURL = "https://api.giphy.com/v1/gifs/search?api_key=30JspaJIb3w1ZakLhJQZgtog3Z26m3mp&q=" + gifQ + "&limit=" + limit + "&offset=0&lang=en";
-    debugger;
-    $.ajax({
-        url: gifURL,
-        method: "GET"
-    }).then(function (response) {
-        for (var i = startSearchNum; i < limit; i++) {
-            $("#gif-view").append("<div>");
-            $("#gif-view div:last-child").attr("class", "divWithGif");
-            $("#gif-view div:last-child").append("<img>");
-            $(".divWithGif img:last-child").attr("src", response.data[i].images.downsized_still.url);
-            $(".divWithGif img:last-child").attr("value", gifQ);
-            $(".divWithGif img:last-child").attr("data-moving", response.data[i].images.downsized_large.url);
-            $(".divWithGif img:last-child").attr("data-static", response.data[i].images.downsized_still.url);
-            $(".divWithGif:last-child").append("<span>");
-            $(".divWithGif span:last-child").text("Rating " + response.data[i].rating);
+    if (limit == 0) {
+        console.log("Put the value");
+    } else {
+        var startSearchNum = $("#gif-view").children().length;
+        limit += 10;
+        gifQ = $(".divWithGif img:first-child").eq(0).attr("value");
+        var gifURL = "https://api.giphy.com/v1/gifs/search?api_key=30JspaJIb3w1ZakLhJQZgtog3Z26m3mp&q=" + gifQ + "&limit=" + limit + "&offset=0&lang=en";
+        $.ajax({
+            url: gifURL,
+            method: "GET"
+        }).then(function (response) {
+            for (var i = startSearchNum; i < limit; i++) {
+                $("#gif-view").append("<div>");
+                $("#gif-view div:last-child").attr("class", "divWithGif");
+                $("#gif-view div:last-child").append("<img>");
+                $(".divWithGif img:last-child").attr("src", response.data[i].images.downsized_still.url);
+                $(".divWithGif img:last-child").attr("value", gifQ);
+                $(".divWithGif img:last-child").attr("data-moving", response.data[i].images.downsized_large.url);
+                $(".divWithGif img:last-child").attr("data-static", response.data[i].images.downsized_still.url);
+                $(".divWithGif:last-child").append("<span>");
+                $(".divWithGif span:last-child").text("Rating " + response.data[i].rating);
 
 
-        }
-    });
+            }
+        });
+    }
 })
+
 
 $("input[type=submit]").on("click", function () {
     var newAnimal = $("input[type=text]").val();

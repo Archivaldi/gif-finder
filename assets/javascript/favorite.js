8ac6d94736db6a31bccd7c68b295e42d0461aae6
Artur Markov <artur.markov1860@gmail.com>
2019-08-01 18:10:07 -0700

    connects with firebase

---
+++ assets/javascript/favorite.js
@@ -0,0 +1,24 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAXHO7kWS5V4Z81zMeyJHJcI1QYOxN8-bA",
+    authDomain: "gif-finder-7967a.firebaseapp.com",
+    databaseURL: "https://gif-finder-7967a.firebaseio.com",
+    projectId: "gif-finder-7967a",
+    storageBucket: "",
+    messagingSenderId: "619759614376",
+    appId: "1:619759614376:web:9edcb3e313100203"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+database.ref().on("value", function(snapshot) {
+    $("div").empty();
+    for (var i = 0; i < snapshot.favoriteGifs.length; i++){
+        console.log(snapshot.favoriteGifs);
+        $("div").append("<img>");
+        console.log(favoriteGifs[i]);
+        $("div img:last-child").attr("src", favoriteGifs[i]);
+    }
+});
